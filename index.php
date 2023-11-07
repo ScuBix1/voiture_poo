2f82c4d95fa1bd1dfbd46f4fd84f4fb9b032758b
Batian21 <bastian.monnin1@gmail.com>
2023-11-07 14:51:09 +0100

    0.1

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 
-print '<pre>';
-print_r($_POST);
-print '</pre>';
+
 
 require 'config/settings.php';
 require 'lib/utilities.php';
@@ -10,46 +8,118 @@ require 'lib/utilities.php';
 // Chargement automatique des classes
 spl_autoload_register('autoloader');
 
-// Instancier les véhicules
+// Initialisation des variables
+$selVehicule = '';
+$color = '';
+// Instanciation de la classe Car
 $car = new Car();
-$truck = new Truck();
+// Instanciation de la classe Truck
+$truck = new Truck;
+
 if (empty($_POST)) {
-    $car->reset();
-    $truck->reset();
-    $vitesse = rand(1, 13)*10;
-}else {
-    if(isset($_POST["vehicule"])){
-        $selVehicule = securisation($_POST["vehicule"]);  
-    }
-    if(isset($_POST["speed"])){
-        $speed = intval(securisation($_POST["speed"]));
-    }
-    if(isset($_POST["color"])){
-        $cssColor = securisation($_POST["color"]);
-        
-    }
-    if($_POST["action"]=="color"){
-        if($selVehicule == "truck"){
-            $truck->setColor($cssColor);
-        }
-        if($selVehicule == "car"){
-            $car->setColor($cssColor);
-        }
-    }
-    if($_POST["action"]==="drive"){
-        if($_POST["vehicule"]==="car"){
-            $car->drive($speed);
-        }else if($_POST["vehicule"] === "truck"){
-            $truck->drive($speed);
-        }
-    }
-    if($_POST["action"]=="reset"){
-        $car->reset();
-        $truck->reset();
-    }
-    var_dump($_POST["action"]);
-    var_dump($_POST["vehicule"]);
+  // Initialisation des propriétés de la voiture
+  $car->reset();
+  // Initialisation des propriétés du camion
+  $truck->reset();
+  // Initialisation de la vitesse par défaut
+  $speed = rand(1, 13) * 10;
+  // Stockage des valeurs d'initialisation dans les cookies
+  setcookie('carcolor', $car->getColor());
+  setcookie('carposition', $car->getPosition());
+  setcookie('cardriver', $car->getDriver());
+  setcookie('carpassengers', $car->getPassengers());
+  setcookie('truckcolor', $truck->getColor());
+  setcookie('truckposition', $truck->getPosition());
+  setcookie('truckdriver', $truck->getDriver());
+  setcookie('truckloading', $truck->getLoading());
 }
+else {
+  // Récupérer les cookies
+  if (!empty($_COOKIE)) {
+    if (isset($_COOKIE['carcolor'])) {
+      $car->setColor($_COOKIE['carcolor']);
+    }
+    if (isset($_COOKIE['carposition'])) {
+      $car->setPosition($_COOKIE['carposition']);
+    }
+    if (isset($_COOKIE['cardriver'])) {
+      $car->setDriver($_COOKIE['cardriver']);
+    }
+    if (isset($_COOKIE['carpassengers'])) {
+      $car->setPassengers($_COOKIE['carpassengers']);
+    }
+    if (isset($_COOKIE['truckcolor'])) {
+      $truck->setColor($_COOKIE['truckcolor']);
+    }
+    if (isset($_COOKIE['truckposition'])) {
+      $truck->setPosition($_COOKIE['truckposition']);
+    }
+    if (isset($_COOKIE['truckdriver'])) {
+      $truck->setDriver($_COOKIE['truckdriver']);
+    }
+    if (isset($_COOKIE['truckloading'])) {
+      $truck->setLoading($_COOKIE['truckloading']);
+    }
+  }
+  // Récupérer les valeurs du formulaire
+  if (isset($_POST['vehicule'])) {
+    $selVehicule = securisation($_POST['vehicule']);
+  }
+  if (isset($_POST['speed'])) {
+    $speed = intval(securisation($_POST['speed']));
+  }
+  if (isset($_POST['color'])) {
+    $color = securisation($_POST['color']);
+  }
+
+  if (isset($_POST['action'])) {
+    if ($_POST['action'] === 'paint') {
+      //printf ('<p>Peindre le véhicule %s</p>', $selVehicule);
+      if ($color !== '') {
+        if ($selVehicule === 'truck') {
+          $truck->setColor($color);
+          setcookie('truckcolor', $color);
+        }
+        else {
+          $car->setColor($color);
+          setcookie('carcolor', $color);
+        }
+      }
+    }
+    elseif ($_POST['action'] === 'drive') {
+      //printf ('<p>Rouler avec le véhicule %s</p>', $selVehicule);
+      if ($selVehicule === 'truck') {
+        $truck->drive($speed);
+        setcookie('truckposition', $truck->getPosition());
+      }
+      else {
+        $car->drive($speed);
+        setcookie('carposition', $car->getPosition());
+      }
+    }
+    elseif ($_POST['action'] === 'reset') {
+      //printf ('<p>Reset du véhicule %s</p>', $selVehicule);
+      if ($selVehicule === 'truck') {
+        $truck->reset();
+        setcookie('truckcolor', $truck->getColor());
+        setcookie('truckposition', $truck->getPosition());
+        setcookie('truckdriver', $truck->getDriver());
+        setcookie('truckloading', $truck->getLoading());
+      }
+      else {
+        $car->reset();
+        setcookie('carcolor', $car->getColor());
+        setcookie('carposition', $car->getPosition());
+        setcookie('cardriver', $car->getDriver());
+        setcookie('carpassengers', $car->getPassengers());
+      }
+    }
+    else {
+      echo '<p>Action non prévue</p>';
+    }
+  }
+}
+
 require 'views/index.html.php';
 
 ?>
